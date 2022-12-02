bdfb94e2d44e6fc2b777b8c39b1d08132becb6dc
jobabyyy <joeannaavila123@gmail.com>
2022-12-02 15:56:33 -0600

    updating functions

--- string.c
+++ string.c
@@ -1,6 +1,7 @@
 #include "main-shell.h"
 #include <string.h>
 
+// STRING LENGTH COPY //
 
 /**
  * _strlen - return length of a string.
@@ -18,20 +19,79 @@ int _strlen(char *s)
 	return (l);
 }
 
-
+// STRING COPY FUNCTION //
 
 /**
- * _strlen - return length of a string.
+ * _strncpy - write a function that copies a string.
  *
- * @s: Int
+ * @dest: Variable1
+ * @src: Variable2
+ * @n: Variable3
  *
- * Return: Value of string.
+ * Return: 0.
  */
-int _strlen(char *s)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int l = 0;
+	int i;
 
-	while (s[l] != '\0')
-		l++;
-	return (l);
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	dest[i] = src[i];
+	for ( ; i < n; i++)
+	dest[i] = '\0';
+	return (dest);
+}
+
+// STRING CONCATENATE FUNCTION //
+
+/**
+ * *_strncat - concatenate 2 strings but do
+ * not include null.
+ *
+ * @dest: char1
+ * @src: char2
+ * @n: char3
+ *
+ * Return: 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dlen = 0;
+	int i;
+
+	while (dest[dlen] != '\0')
+	{
+		dlen++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++, dlen++)
+	{
+		dest[dlen] = src[i];
+	}
+	dest[dlen] = '\0';
+	return (dest);
+}
+
+// STRING COMPARE VALUE FUNCTION //
+
+/**
+ * _strcmp - compare two strings
+ *
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: compared values
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, b;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0')
+	{
+		i++;
+	}
+
+	b = s1[i] - s2[i];
+	return (b);
 }
