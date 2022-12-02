37309ef11acdd64fdf5cf22fbef584a6bcc58e3c
jobabyyy <joeannaavila123@gmail.com>
2022-12-02 15:40:15 -0600

    Created string functions

---
+++ string.c
@@ -0,0 +1,37 @@
+#include "main-shell.h"
+#include <string.h>
+
+
+/**
+ * _strlen - return length of a string.
+ *
+ * @s: Int
+ *
+ * Return: Value of string.
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+		l++;
+	return (l);
+}
+
+
+
+/**
+ * _strlen - return length of a string.
+ *
+ * @s: Int
+ *
+ * Return: Value of string.
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+		l++;
+	return (l);
+}
