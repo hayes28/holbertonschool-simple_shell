7856a07f49a51a48374b4aba9b707a2ee9dc7a33
jobabyyy <joeannaavila123@gmail.com>
2022-12-06 12:45:00 -0800

    created exit function

--- main_shell.c
+++ main_shell.c
@@ -13,15 +13,27 @@ int main(int ac, char **argv)
 	char *prompt = "(holbertonschool-simple_shell) $ "; /*creating prompt line for user*/
 	char *lineptr;
 	size_t n = 0;
-	
-	(void)ac;
-	(void)argv;
+	ssize_t nchars_read;
 
+	(void)ac;
+	(void)argv;				/* delcaring void variables */
+
+	while (1)
+	{					/* creating infinite loop to execute until user stops*/
 
 	printf("%s", prompt);
-	getline(&lineptr, &n, stdin);
+	nchars_read = getline(&lineptr, &n, stdin); /*checks getline function success or when EOF is reached*/
+
+	if (nchars_read == -1)
+	{
+		printf("Shell is exiting...\n");
+		return (-1); 			/*return -1 if exit fail*/
+	}
+
 	printf("%s\n", lineptr);
 
-	free(lineptr);
-	return (0);		
+	free(lineptr);				/*frees allocated memory*/
+	}
+
+	return (0);
 }
